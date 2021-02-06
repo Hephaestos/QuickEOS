e01466b1a255eefc4d63856eb2c12282fb529d44
Hephaestos <thedanielfloyd@protonmail.com>
2021-02-06 00:04:54 -0500

    add flags to choose server number and to select arch

--- QuickEOS.ps1
+++ QuickEOS.ps1
@@ -1,3 +1,25 @@
+param(
+    [int32]$n = 01,
+    [switch]$arch = $false
+    [switch]$help = $false
+    [switch]$h = $false
+)
+if ($arch){
+    if($n -lt 1 -or $n -gt 10) {
+        Write-Host "QuickEOS: Invalid server number" -fore red
+        Exit
+    }
+} else {
+    if($n -lt 1 -or $n -gt 32) {
+        Write-Host "QuickEOS: Invalid server number" -fore red
+        Exit
+    }
+}
+$number = $n
+if ($n -lt 10) {
+    $number = "0"+$n
+}
+
 $vpn = "C:\Program Files (x86)\Common Files\Pulse Secure\Integration\pulselauncher.exe"
 $vpnexists = Test-Path -Path $vpn
 if (-not $vpnexists) {
@@ -16,13 +38,16 @@ if($vpncred) {
     $username = $vpncred.Username
     $passwd = $vpncred.Password
     $plainpasswd = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwd))
+    Write-Host = "QuickEOS: Connecting VPN" -fore green
     & $vpn -u $username -p $plainpasswd -url vpn.student.gvsu.edu -r Users
     if ($lastexitcode -eq 0) {
         if (-not $credsexist) {
             Write-Host "QuickEOS: Storing credentials to ${credpath}" -fore green
             $vpncred | Export-CliXml -Path $credpath
         }
-        ssh "${username}@eos01.cis.gvsu.edu"
+        $lab = if ($arch) {"arch"} else {"eos"}
+        ssh "${username}@${lab}${number}.cis.gvsu.edu"
+        Write-Host = "QuickEOS: Disconnecting VPN" -fore green
         & $vpn -stop
     } else {
         Write-Host "QuickEOS: VPN Connection Error" -fore red
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # QuickEOS
-v1.0
+v1.1
 
 A little script to quickly log into the GVSU EOS servers using Pulse VPN
 
-Suggested Use:
+### Usage:
+`./QuickEOS.ps1 [-n <int>] [-arch]`
+Use `-n` to specify the number of the server you want to connect to (eos01, eos02, etc.)
+Use `-arch` to connect to arch servers instead of eos
+
+### Suggested Installation:
 - Put it in `~/Documents/WindowsPowerShell/Scripts`
 - Add an alias to your `$PROFILE` like `Set-Alias -Name QuickEOS -Value ~\Documents\WindowsPowerShell\Scripts\QuickEOS.ps1`
 
@@ -11,5 +16,5 @@ Or however you like, don't let me tell you what to do.
 If you don't like my very mediocre PowerShell scripting, send me a PR.
 Enjoy :)
 
-!!! Disclaimer !!!
+### !!! Disclaimer !!!
 This will store your GVSU credentials to your device. Use at your own risk.
